8745d2ac782c2fd43fb31f7c9ab15ea26595a9e3
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2023-03-26 21:54:35 +0200

    Playing with styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button color={'red'} title={'Some text'} />
+
+      <TextInput placeholder={'Put some text'}/>
+      <TextInput multiline placeholder={'Put more texts'} style={styles.textInput} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,10 +18,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'grey',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
   },
-  text: {
-    color: 'orange'
+  button: {
+    textTransform: 'lowercase',
+    backgroundColor: 'black'
+  },
+  textInput: {
+    // width: "50%",
+    borderBottomColor: 'white'
   }
 });
